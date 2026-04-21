e1dac17ff67a477241eb04d2a95afa957fda3dc1
Serg <serg.butenko@gmail.com>
2026-04-21 18:57:32 +0300

    v 2.0.1: stylesheets under construction

--- style.cpp
+++ style.cpp
@@ -12,14 +12,10 @@ QString Style::label()
 QString Style::pushButton(bool checkable)
 {
     QString style;
-    if (checkable)    {
-        style += "QPushButton {background-color: rgb(66, 85, 138); color: white; border: none;} ";
-        style += "QPushButton:checked {background-color: rgb(26, 45, 198); color: white;} ";
-    } else {
-        style = "QPushButton {background-color: rgb(66, 85, 138); color: white;} ";
+        style = "QPushButton {background-color: rgb(66, 85, 138); color: white; ";
+        style += " border-radius: 3px; border: 1px solid rgb(26, 45, 98); padding: 2px;} ";
         style += "QPushButton:disabled {background-color: rgb(2, 9, 12); color: gray;} ";
-        style += "QPushButton:checked {background-color: rgb(6, 13, 37); color: white;} ";
-    }
+        style += "QPushButton:checked {background-color: rgb(26, 45, 240); color: white;} ";
     return style;
 }
 
@@ -37,7 +33,7 @@ QString Style::tabWidget()
 {
     QString style;
 
-    style += "QTabWidget::pane {background-color: #2F2F2F; border: 1px solid #C2C7CB; }";
+    style += "QTabWidget::pane {background-color: rgb(18, 18, 18); border: 1px solid rgb(76, 78, 80); }";
     style += "QTabBar::tab {background: rgb(26, 45, 98); color: white; } ";
     style += "QTabBar::tab:hover {background: rgb(126, 145, 198); color: gray; } ";
     style += "QTabBar::tab:disabled {background: rgb(26, 45, 98); color: white; }";
@@ -77,12 +73,12 @@ QString Style::tableWidget()
 {
     QString style;
 
-    style = "QTableWidget {background-color: #2F2F2F; border: 1px solid #4181C0;color: white;} "
+    style = "QTableWidget {background-color: rgb(18, 18, 18); border: 1px solid rgb(25, 51, 75);color: white;} "
             "QTableWidget::item:selected {background-color: rgb(26, 45, 198); color: white;} "
             "QTableWidget::indicator:checked {image: url(:/new/prefix1/checked.png);} "
             "QTableWidget::indicator:unchecked {image: url(:/new/prefix1/unchecked.png);} "
         ;
-    style += "QScrollBar:horizontal { height: 5px; background: rgb(26, 45, 98); border: 1px solid green;} ";
+    style += "QScrollBar:horizontal { height: 5px; background: rgb(26, 45, 98); ;} ";
     style += "QScrollBar::handle:horizontal {background: rgb(26, 45, 198); min-width: 20px;} ";
     style += "QScrollBar:vertical { width: 5px; background: rgb(26, 45, 98); } ";
     style += "QScrollBar::handle:vertical {background: rgb(26, 45, 198); min-width: 20px;} ";
@@ -94,15 +90,15 @@ QString Style::listWidget()
     QString style;
 
     style = "QListWidget {"
-            "background-color: #2F2F2F;"
-            "border: 1px solid #4181C0;"
+            "background-color: rgb(18, 18, 18);"
+            "border: 1px solid rgb(25, 51, 75);"
             "color: white;"
-            "selection-background-color: #5191D0; /* Color when an item is selected */"
+            "selection-background-color: rgb(32, 60, 82); /* Color when an item is selected */"
             "selection-color: white;"
             "} "
-            "QListWidget::item:selected { background-color: #0078d7; color: white;} ";
+            "QListWidget::item:selected { background-color: rgb(0, 41, 84); color: white;} ";
 
-    style += "QScrollBar:horizontal { height: 5px; background: rgb(26, 45, 98); border: 1px solid green;} ";
+    style += "QScrollBar:horizontal { height: 5px; background: rgb(26, 45, 98);} ";
     style += "QScrollBar::handle:horizontal {background: rgb(26, 45, 198); min-width: 20px;} ";
     style += "QScrollBar:vertical { width: 5px; background: rgb(26, 45, 98); } ";
     style += "QScrollBar::handle:vertical {background: rgb(26, 45, 198); min-width: 20px;} ";
@@ -145,7 +141,7 @@ QString Style::toolButton()
 QString Style::comboBox()
 {
     QString style;
-    style += "QComboBox {background-color: rgb(1, 70, 100); color: white; border: 1px solid #555; padding: 5px;} ";
+    style += "QComboBox {background-color: rgb(1, 70, 100); color: white; border: 1px solid rgb(33, 33, 33); padding: 5px;} ";
     style += "QComboBox QAbstractItemView {background-color: rgb(1, 70, 100);color: white; "
              "selection-background-color: #0078d7; selection-color: white;} ";
     return style;
@@ -155,15 +151,15 @@ QString Style::progressBar()
 {
     QString style = R"(
     QProgressBar {
-        border: 1px solid #555;
+        border: 1px solid rgb(33, 33, 33);
         border-radius: 5px;
-        background-color: #2b2b2b;
+        background-color: rgb(17, 17, 17);
         text-align: center;
         color: white;
     }
 
     QProgressBar::chunk {
-        background-color: #00aaff;
+        background-color: rgb(0, 68, 100);
         border-radius: 5px;
     }
     )";
@@ -175,34 +171,34 @@ QString Style::slider()
     QString style = R"(
     QSlider::groove:horizontal {
         height: 4px;
-        background: #2b2b2b;
+        background: rgb(17, 17, 17);
         border-radius: 3px;
     }
 
     QSlider::sub-page:horizontal {
-        background: #00aaff;
+        background: rgb(0, 68, 100);
         border-radius: 1px;
     }
 
     QSlider::add-page:horizontal {
-        background: #444;
+        background: rgb(33, 33, 33);
         border-radius: 1px;
     }
 
     QSlider::handle:horizontal {
-        background: #00aaff;
-        border: 1px solid #0077aa;
+        background: rgb(0, 68, 100);
+        border: 1px solid rgb(0, 47, 67);
         width: 5px;
         margin: -5px 0;
         border-radius: 5px;
     }
 
     QSlider::handle:horizontal:hover {
-        background: #33bbff;
+        background: rgb(20, 73, 100);
     }
 
     QSlider::handle:horizontal:pressed {
-        background: #0088cc;
+        background: rgb(0, 53, 80);
     }
     )";
     return style;
@@ -210,14 +206,14 @@ QString Style::slider()
 
 QString Style::dialog()
 {
-    return "QDialog{background-color: #2F2F2F;} ";
+    return "QDialog{background-color: rgb(18, 18, 18);} ";
 }
 
 QString Style::messageBox()
 {
     QString style = R"(
     QMessageBox {
-        background-color: #2b2b2b;
+        background-color: rgb(18, 18, 18);
     }
 
     QMessageBox QLabel {
@@ -227,18 +223,18 @@ QString Style::messageBox()
     QMessageBox QPushButton {
         background-color: rgb(26, 45, 98);
         color: white;
-        border: 1px solid #666;
+        border: 1px solid rgb(33, 33, 33);
         padding: 5px 12px;
         border-radius: 4px;
         min-width: 80px;
     }
 
     QMessageBox QPushButton:hover {
-        background-color: #555;
+        background-color: rgb(33, 33, 33);
     }
 
     QMessageBox QPushButton:pressed {
-        background-color: #333;
+        background-color: rgb(33, 33, 33);
     }
     )";
     return style;
@@ -248,26 +244,26 @@ QString Style::fileDialog()
 {
     QString style = (R"(
     QFileDialog {
-        background-color: #2b2b2b;
-        color: #ffffff;
+        background-color: rgb(18, 18, 18);
+        color: white;
     }
     QPushButton {
-        background-color: #444;
+        background-color: rgb(33, 33, 33);
         color: white;
-        border: 1px solid #666;
+        border: 1px solid rgb(33, 33, 33);
         padding: 5px;
     }
     QPushButton:hover {
-        background-color: #555;
+        background-color: rgb(33, 33, 33);
     }
     QLineEdit {
-        background-color: #3c3c3c;
+        background-color: rgb(24, 24, 24);
         color: white;
-        border: 1px solid #666;
+        border: 1px solid rgb(33, 33, 33);
     }
     QListView, QTreeView {
-        background-color: #2b2b2b;
-        alternate-background-color: #3c3c3c;
+        background-color: rgb(18, 18, 18);
+        alternate-background-color: rgb(24, 24, 24);
     }
     )");
     return style;
